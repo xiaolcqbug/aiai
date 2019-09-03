63cae97ce66e7f5a99f21c6470e104ecac08d1b4
author <author@corpmail.com>
2019-09-03 17:04:50 +0800

    tangjinhao

---
+++ 唐金浩/唐金浩八月考试/网络通信udp.py
@@ -0,0 +1,16 @@
+import socket
+
+s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+s.bind(('127.0.0.1',9999))
+
+print('Bind UDP on 9999...')
+while True:
+    data,addr=s.recvfrom(1024)
+    print('Received from %s:%s.'% addr)
+    s.sendto(b'Hello,%s!'% data,addr)
+
+s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+for data in [b'Michael',b'Tracy',b'Sarah']:
+    s.sendto(data,('127.0.0.1',9999))
+    print(s.recv(1024).decode('utf-8'))
+s.close()
